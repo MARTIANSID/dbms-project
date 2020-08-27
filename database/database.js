9dfbb2c2a36ac37f5e1b2e7872250df0bb2450e3
rishabhmishra23599 <52506383+rishabhmishra23599@users.noreply.github.com>
2020-08-27 23:59:21 +0530

    commented

--- database/database.js
+++ database/database.js
@@ -3,9 +3,10 @@ const mysql=require('mysql2');
 const db =mysql.createConnection({
     host :'localhost',
     user:'root',
-    password:'manGla1232',
+    password:'manGla1232', //Abbey dc loool dekh maine thode edits kiye lol
     database:'delivery'
 })
+
 db.connect((err=>{
     if(err){
         throw err;
