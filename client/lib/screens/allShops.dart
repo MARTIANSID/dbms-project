4e7dd900060067b7bf030b27a773ea307bfc296e
MARTIANSID <59787009+MARTIANSID@users.noreply.github.com>
2020-10-22 13:45:47 +0530

    Update allShops.dart

--- client/lib/screens/allShops.dart
+++ client/lib/screens/allShops.dart
@@ -8,15 +8,47 @@ class AllShops extends StatefulWidget {
 }
 
 class _AllShopsState extends State<AllShops> {
+  bool isLoading = false;
   @override
   void initState() {
     Future.delayed(Duration.zero, () async {
+      setState(() {
+        isLoading = true;
+      });
       await Provider.of<CustomerView>(context, listen: false).getAllShops();
+      setState(() {
+        isLoading = false;
+      });
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text('All Shops')));
+    return Scaffold(
+        body: isLoading
+            ? CircularProgressIndicator()
+            : ListView.builder(
+                itemCount: Provider.of<CustomerView>(context, listen: false)
+                    .shops
+                    .length,
+                itemBuilder: (context, i) {
+                  return Card(
+                    child: Container(
+                      child: Column(
+                        children: [
+                          Text(Provider.of<CustomerView>(context, listen: false)
+                              .shops[i]
+                              .shopName),
+                          Text(Provider.of<CustomerView>(context, listen: false)
+                              .shops[i]
+                              .address),
+                          Text(Provider.of<CustomerView>(context, listen: false)
+                              .shops[i]
+                              .category)
+                        ],
+                      ),
+                    ),
+                  );
+                }));
   }
 }
